7df5443a935059d472b921b8c806895fce51e1f5
btemovska <btemovska@gmail.com>
2020-06-18 12:57:18 -0400

    adding content

--- src/main/java/com/example/reviews/ReviewRepository.java
+++ src/main/java/com/example/reviews/ReviewRepository.java
@@ -13,11 +13,11 @@ public class ReviewRepository {
 
     public ReviewRepository() {
         Review Spiderman = new Review(1L, "Spiderman", "./images/spiderMan.jpg",
-                "movies", "fictional superhero");
+                "Movies", "\"Spider-Man\" centers on student Peter Parker (Tobey Maguire) who, after being bitten by a genetically-altered spider, gains superhuman strength and the spider-like ability to cling to any surface. He vows to use his abilities to fight crime, coming to understand the words of his beloved Uncle Ben: With great power comes great responsibility.");
         Review theGodfather = new Review(2L, "The Godfather", "./images/theGodfather.jpg",
-                "movies", "one of the greatest films of all times");
+                "Movies", "Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando). When the don's youngest son, Michael (Al Pacino), reluctantly joins the Mafia, he becomes involved in the inevitable cycle of violence and betrayal. Although Michael tries to maintain a normal relationship with his wife, Kay (Diane Keaton), he is drawn deeper into the family business.");
         Review captainAmerica = new Review(3L, "Captain America", "./images/captainAmerica.jpg",
-                "movies", "fictional superhero");
+                "Movies", "It is 1941 and the world is in the throes of war. Steve Rogers (Chris Evans) wants to do his part and join America's armed forces, but the military rejects him because of his small stature. Finally, Steve gets his chance when he is accepted into an experimental program that turns him into a supersoldier called Captain America. Joining forces with Bucky Barnes (Sebastian Stan) and Peggy Carter (Hayley Atwell), Captain America leads the fight against the Nazi-backed HYDRA organization.");
 
         reviewList.put(Spiderman.getId(), Spiderman);
         reviewList.put(theGodfather.getId(), theGodfather);
